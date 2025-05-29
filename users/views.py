073e038b5e3d3e03e5ceb6bd75b2abce5b8d4ac2
pouya000 <puya.shamsi@gmail.com>
2025-05-30 00:06:56 +0330

    ad users/views.py

--- users/views.py
+++ users/views.py
@@ -114,7 +114,7 @@ class LoginView(APIView):
             'iat': datetime.datetime.utcnow()
         }
 
-        token = jwt.encode(payload, 'secret', algorithm='HS256', SameSite=None, Secure=True)
+        token = jwt.encode(payload, 'secret', algorithm='HS256', samesite=None, secure=True)
         response = Response()
         response.set_cookie(key='jwt', value=token, httponly=True, max_age=60000)
 
